74229567ff2d02f0da9b27a5c966609774ecc7f1
ChristopherEvers <chrisevers98@gmail.com>
2020-03-11 15:43:00 -0400

    Weather app using api

---
+++ pages/weather/js/app.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(
+    '.temperature-description',
+  );
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
+  let icon = document.querySelector('.icon');
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://weatherbit-v1-mashape.p.rapidapi.com/forecast/hourly?lang=en&hours=120&lat=${lat}&lon=${long}`;
+      fetch(api, {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com',
+          'x-rapidapi-key':
+            '0b9b97bcecmsh03c284633a2ee9dp1ab800jsn6fedfe2034b0',
+        },
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          // console.log(data);
+          const {temp, weather} = data.data[0];
+          //set DOM elements to location
+          temperatureDegree.textContent = ((temp * 9) / 5 + 32).toFixed(0);
+          temperatureDescription.textContent = weather.description;
+          locationTimezone.textContent = data.city_name;
+          icon.innerHTML = `<img src="https://www.weatherbit.io/static/img/icons/${weather.icon}.png">`;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  }
+});
